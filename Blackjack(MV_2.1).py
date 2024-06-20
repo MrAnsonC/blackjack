40a4abf1ced3ccb0b4b16caa7c3fd90fabafe95e
HSC <g3gt26@gmail.com>
2024-06-21 09:26:57 +1200

    Update to MV_2.1

--- Blackjack(MV_2.1).py
+++ Blackjack(MV_2.1).py
@@ -48,6 +48,7 @@ Upload PCV_7 on GitHub in 21 of June, 2024. 0000(NZST, GMT+12)
 Upload MV_2.1 on GitHub in 21 of June, 2024. 0925(NZST, GMT+12)
 -- PCV_7 will be update soon.
 -- Player get same colour of PP, 12:1 available.
+
 =========================================================================
 The latest version:  MV_2.1    PCV_7
 '''
