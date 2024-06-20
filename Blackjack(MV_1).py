1badb2b6e24bd47172d880d93b9da915cbc0550d
HSC <g3gt26@gmail.com>
2024-06-20 21:06:46 +1200

    Forget to delete Debug setting

--- Blackjack(MV_1).py
+++ Blackjack(MV_1).py
@@ -39,8 +39,8 @@ import random
 
 # Define card suits and ranks
 suits = ['♥', '♦', '♣', '♠']
-##ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-ranks = ['K', 'A']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+##ranks = ['K', 'A']
 
 # Define card class
 class Card:
